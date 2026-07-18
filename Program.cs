6c59fc2d12cf0c9aed888099f39a41d81989f3bd
Sibaw56 <161341965+Sibaw56@users.noreply.github.com>
2026-07-19 00:12:34 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,26 +1,60 @@
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
-
+builder.Services.AddSingleton<MongoDBService>();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromHours(2);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+try
+{
+    var client = new MongoClient(builder.Configuration["MongoDB:ConnectionURI"]);
+
+    var database = client.GetDatabase("DorMap");
+    var students = database.GetCollection<studentUser>("Students");
+    var student = students.Find(s => s.Id == 1).FirstOrDefault();
+
+    if (student != null)
+    {
+        Console.WriteLine("Connected to MongoDB! yay");
+        Console.WriteLine($"Student: {student.FirstName} {student.LastName} {student.Email} {student.Phone} {student.DateJoined}");
+    }
+    else
+    {
+        Console.WriteLine("Connected to MongoDB! yay");
+        Console.WriteLine("No student with Id = 1 was found but still connected.");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Connection failed. u stinky");
+    Console.WriteLine(ex.ToString());
+}
+
 
 app.UseHttpsRedirection();
 
 app.UseRouting();
+
+app.UseSession();
 
 app.UseAuthorization();
 
 app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
-
 app.Run();
+//real real real real real
